7b4bbc29d67033368c6daebe789dd59e88e7b1a2
Robinson Merillat <rdmerillat@mymail.mines.edu>
2017-10-18 03:06:02 -0600

    updated margin on cubes

--- src/app.rs
+++ src/app.rs
@@ -156,10 +156,9 @@ impl<R: gfx::Resources> App<R> {
         for i in 0..10 {
             for j in 0..10 {
                 for k in 0..10 {
-
                     let mut body = RigidBody::new_dynamic(Cuboid::new(Vector3::new(1.0, 1.0 , 1.0) * 0.04), 1000., 0.65, 0.47);
-
-                    body.set_transformation(Isometry3::new(Vector3::new(i as f32, j as f32 + 0.001, k as f32) * 0.081, na::zero()));
+                    body.set_margin(0.00001);
+                    body.set_transformation(Isometry3::new(Vector3::new(i as f32, j as f32 + 0.001, k as f32) * 0.041, na::zero()));
 
                     self.obj_list.push(Object{
                         body: self.physics_world.add_rigid_body(body),
